76cfe6b88414105005686dd8687df6a1e98b75c1
minwoo00044 <minwoo0004@gmail.com>
2023-09-10 17:28:43 +0900

    feat playerInteractConsole
    콘솔과 상호작용하는 기능 초반부

---
+++ Assets/Minwoo/Scripts/PlayerInteraction.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float maxDistance;
+    public LayerMask interactableObject;
+    public GameObject Guidetext;
+
+    private  GameObject _targetUI;
+    void Update()
+    {
+        Vector3 viewportCenter = new Vector3(0.5f, 0.5f, 0.0f);
+
+        // Cast a ray from the center of the camera
+        Ray ray = Camera.main.ViewportPointToRay(viewportCenter);
+
+        RaycastHit hit;
+        if (Guidetext != null)
+        {
+            if (Physics.Raycast(ray, out hit, maxDistance, interactableObject))
+            {
+                if (hit.collider != null)
+                {
+                    Guidetext.SetActive(true);
+                    _targetUI = hit.collider.gameObject.GetComponent<InteractableObject>().targetUI;
+                }
+            }
+            else
+            {
+                Guidetext.SetActive(false);
+                _targetUI = null;
+            }
+        }
+
+        if(_targetUI != null)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                _targetUI.SetActive(true);
+            }
+        }
+    }
+}
