548b28c7d20c0a49a964008fb2833df1447530e0
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2023-09-12 18:15:03 +0900

    cage temp
    집가서함

--- Assets/Minwoo/Scripts/CageConsole.cs
+++ Assets/Minwoo/Scripts/CageConsole.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class CageConsole : MonoBehaviour
 {
-    public void Test()
+    public GameObject[] buildingList;
+
+    public GameObject center;
+    private int _buildingIndex;
+    public int buildingIndex
     {
-        print("test");
+        get { return _buildingIndex; }
+        set 
+        {
+            _buildingIndex = value;
+            Build();
+        }
+    }
+
+    private void Build()
+    {
+        GameObject instance = Instantiate(buildingList[buildingIndex]);
+        instance.transform.position = center.transform.GetChild(0).transform.position;
+
     }
 }
