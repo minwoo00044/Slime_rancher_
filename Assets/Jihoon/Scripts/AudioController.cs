442041909451cbd02d6501f74b84b5ecfa5ef67f
ththrh <44047525+ththrh@users.noreply.github.com>
2023-09-14 01:13:44 +0900

    AudioMixer, Audiocontroller
    오디오믹서 - 오디오소스들에 output : master 할당필요
    Audiocontroller - 소리 조절, 슬라이더에 내용 넣어야함

---
+++ Assets/Jihoon/Scripts/AudioController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioControl : MonoBehaviour
+{
+    public AudioMixer master;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
