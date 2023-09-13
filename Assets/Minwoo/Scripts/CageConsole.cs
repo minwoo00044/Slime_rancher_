f4ef71e3363acd011071f0b71e4a5aa7f4a55118
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2023-09-13 11:12:10 +0900

    fix 기능강화 골드지불
    이제 기능 강화도 돈나갑니다.

--- Assets/Minwoo/Scripts/CageConsole.cs
+++ Assets/Minwoo/Scripts/CageConsole.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CageConsole : MonoBehaviour
 {
@@ -17,12 +20,31 @@ public class CageConsole : MonoBehaviour
             Build();
         }
     }
+    private Dictionary<GameObject, int> _bill = new Dictionary<GameObject, int>();
 
+    private void Awake()
+    {
+        _bill.Add(buildingList[0], 250);
+    }
     private void Build()
     {
-        GameObject instance = Instantiate(buildingList[buildingIndex], center.GetChild(0));
-        Vector3 instancePos = new Vector3(instance.transform.parent.GetChild(1).position.x + 1, instance.transform.parent.GetChild(1).position.y  , instance.transform.parent.GetChild(1).position.z);
-        instance.transform.position = instancePos;
+        if (StatusUIManager.instance.gold >= _bill[buildingList[_buildingIndex]])
+        {
+            GameObject instance = Instantiate(buildingList[_buildingIndex], center.GetChild(0));
+            Vector3 instancePos = new Vector3(instance.transform.parent.GetChild(buildingIndex + 1).position.x + 1, instance.transform.parent.GetChild(1).position.y, instance.transform.parent.GetChild(1).position.z);
+            instance.transform.position = instancePos;
+        }
+        else
+        {
+            GameObject clickObject = EventSystem.current.currentSelectedGameObject;
+            StartCoroutine(swtichButton(clickObject.GetComponent<Button>()));
+        }
+    }
 
+    IEnumerator swtichButton(Button button)
+    {
+        yield return new WaitForSeconds(0.5f);
+        print(button.name);
+        button.interactable = true;
     }
 }
